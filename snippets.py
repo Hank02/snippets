1b50dbbac878236a60c6f6e1ea7506a4e3704229
Hank02 <enriqueqw@gmail.com>
2015-12-23 17:40:30 +0000

    Work in progress

--- snippets.py
+++ snippets.py
@@ -1,4 +1,6 @@
 import logging
+import argparse
+import sys
 
 # set the log output file and the log level
 logging.basicConfig(filename = "snippets.log", level = logging.DEBUG)
@@ -14,3 +16,20 @@ def get(name):
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
     # return snippet
     return ""
+
+def main():
+    logging.info("Construction parser")
+    parser = argparse.ArgumentParser(description = "Store and retreive snippets of text")
+    
+    subparsers = parser.add_subparsers(dest = "command", help = "Available commands")
+    
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help = "Store a snippet")
+    put_parser.add_argument("name", help = "The name of the snippet")
+    put_parser.add_argument("snippet", help = "The snippet of text")
+    
+    arguments = parser.parse_args(sys.argv[1:])
+    
+    
+if __name__ == "__main__":
+    main()
